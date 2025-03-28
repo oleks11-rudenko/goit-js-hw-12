3ce673e4be02b4af832991c73476753343ddaddd
oleks11-rudenko <oleksii-rudenko@ukr.net>
2025-03-28 15:09:08 +0200

    Made some little changes

--- src/main.js
+++ src/main.js
@@ -1,34 +1,36 @@
-import { requestData } from "./js/pixabay-api";
-import { renderMarkup, clearGallery } from "./js/render-functions";
+import { requestData } from './js/pixabay-api';
+import { renderMarkup, clearGallery } from './js/render-functions';
 
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-let request = "";
+let request = '';
 let page = 1;
 let perPage = 15;
 
 const refs = {
-  form: document.querySelector(".form"),
-  gallery: document.querySelector(".gallery"),
-  loader: document.querySelector(".loader"),
-  loadMoreButton: document.querySelector(".loadmore-button"),
+  form: document.querySelector('.form'),
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+  loadMoreButton: document.querySelector('.loadmore-button'),
 };
 
-refs.loadMoreButton.classList.add("hidden");
-
-refs.form.addEventListener("submit", onSumbit);
-refs.loadMoreButton.addEventListener("click", loadMorePictures);
+refs.form.addEventListener('submit', onSumbit);
+refs.loadMoreButton.addEventListener('click', loadMorePictures);
 
 async function onSumbit(event) {
   event.preventDefault();
   request = event.currentTarget[0].value.trim();
+
   const form = event.currentTarget;
+
+  refs.loadMoreButton.classList.add('hidden');
+  refs.loader.classList.remove('hidden');
 
   if (!request) {
     iziToast.warning({
-      message: "Sorry, there are no keywords. Please enter them.",
-      position: "topRight",
+      message: 'Sorry, there are no keywords. Please enter them.',
+      position: 'topRight',
     });
     return;
   }
@@ -41,16 +43,16 @@ async function onSumbit(event) {
     if (hits.length === 0) {
       iziToast.show({
         message:
-          "Sorry, there are no images matching your search query. Please try again!",
-        messageColor: "#FAFAFB",
-        backgroundColor: "#EF4040",
-        iconColor: "#FAFAFB",
-        iconUrl: "./img/octagon.svg",
-        position: "topRight",
+          'Sorry, there are no images matching your search query. Please try again!',
+        messageColor: '#FAFAFB',
+        backgroundColor: '#EF4040',
+        iconColor: '#FAFAFB',
+        iconUrl: './img/octagon.svg',
+        position: 'topRight',
         close: false,
         buttons: [
           [
-            "<button>✕</button>",
+            '<button>✕</button>',
             function (instance, toast) {
               instance.hide({}, toast);
             },
@@ -60,28 +62,28 @@ async function onSumbit(event) {
       return;
     }
     renderMarkup(refs.gallery, hits);
-    refs.loadMoreButton.classList.remove("hidden");
+    refs.loadMoreButton.classList.remove('hidden');
 
     if (page * perPage >= totalHits) {
-      refs.loadMoreButton.classList.add("hidden");
+      refs.loadMoreButton.classList.add('hidden');
       iziToast.warning({
         message: "We're sorry, but you've reached the end of search results.",
-        icon: "",
-        position: "topRight",
+        icon: '',
+        position: 'topRight',
       });
     }
   } catch (error) {
     console.error(error.message);
   } finally {
-    refs.loader.classList.add("hidden");
+    refs.loader.classList.add('hidden');
   }
   form.reset();
 }
 
 async function loadMorePictures() {
   page += 1;
-  refs.loader.classList.remove("hidden");
-  refs.loadMoreButton.classList.add("hidden");
+  refs.loader.classList.remove('hidden');
+  refs.loadMoreButton.classList.add('hidden');
 
   try {
     const { hits, totalHits } = await requestData(request, page, perPage);
@@ -90,26 +92,27 @@ async function loadMorePictures() {
     scrollPage();
 
     if (page * perPage >= totalHits) {
-      refs.loadMoreButton.style.display = "none";
+      refs.loadMoreButton.style.display = 'none';
       iziToast.warning({
         message: "We're sorry, but you've reached the end of search results.",
-        position: "topRight",
+        position: 'topRight',
       });
     } else {
-      refs.loadMoreButton.classList.remove("hidden");
+      refs.loadMoreButton.classList.remove('hidden');
     }
   } catch (error) {
     console.error(error.message);
   } finally {
-    refs.loader.classList.add("hidden");
+    refs.loader.classList.add('hidden');
   }
 }
+
 function scrollPage() {
   const cardHeigth = document
-    .querySelector(".gallery-item")
+    .querySelector('.gallery-item')
     .getBoundingClientRect().height;
   scrollBy({
     top: cardHeigth * 2,
-    behavior: "smooth",
+    behavior: 'smooth',
   });
 }
